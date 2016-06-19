32f21920f0be03a943f0dbfe4f8f2a5a22158b50
eduardohk <eduardohk@users.noreply.github.com>
2016-06-19 08:40:00 -0300

    Update script.sql

--- script/script.sql
+++ script/script.sql
@@ -1,28 +1,50 @@
 CREATE TABLE usuario(
-	u_nome 		varchar(30) 	not null,
-	u_senha 	varchar(30) 	not null,
-	u_celular 	varchar(20) 	not null,
-	constraint u_pk
-		primary key(u_nome)
+	u_nome 		VARCHAR(30) 	NOT NULL,
+	u_senha 	VARCHAR(30) 	NOT NULL,
+	u_celular 	VARCHAR(20) 	NOT NULL,
+	CONSTRAINT u_pk
+		PRIMARY KEY(u_nome)
 );
 
 CREATE TABLE conta(
-	c_id 		serial 		not null,
-	c_nome 		varchar(30) 	not null,
-	c_valor_total 	money		not null,
-	constraint c_pk 
-		primary key(c_id)
+	c_id 		SERIAL 		NOT NULL,
+	c_nome 		VARCHAR(30) 	NOT NULL,
+	c_valor		MONEY		NOT NULL,
+	CONSTRAINT c_pk 
+		PRIMARY KEY(c_id)
+);
+
+CREATE TABLE produto(
+	p_id 		SERIAL		NOT NULL,
+	p_nome		VARCHAR(30)	NOT NULL,
+	p_valor		MONEY		NOT NULL,
+	c_id		INTEGER		NOT NULL,
+	CONSTRAINT p_pk
+		PRIMARY KEY(p_id),
+	CONSTRAINT c_id_fk
+		FOREIGN KEY (c_id) REFERENCES conta(c_id)
 );
 
 CREATE TABLE usuario_conta(
-	u_nome 			varchar(30) 	not null,
-	c_id			integer		not null,
-	u_c_gerente		varchar(30)	not null,
-	u_c_valor_usuario	money		not null,
-	constraint u_c_pk 
-		primary key(u_nome, c_id),
-	constraint u_nome_fk 
-		foreign key (u_nome) references usuario(u_nome),
-	constraint c_id_fk 
-		foreign key (c_id) references conta(c_id)
+	u_nome 		VARCHAR(30) 	NOT NULL,
+	c_id		INTEGER		NOT NULL,
+	u_c_gerente	VARCHAR(30)	NOT NULL,
+	u_c_valor	MONEY		NOT NULL,
+	CONSTRAINT u_c_pk 
+		PRIMARY KEY(u_nome, c_id),
+	CONSTRAINT u_nome_fk 
+		FOREIGN KEY (u_nome) REFERENCES usuario(u_nome),
+	CONSTRAINT c_id_fk 
+		FOREIGN KEY (c_id) REFERENCES conta(c_id)
+);
+
+CREATE TABLE produto_usuario(
+	p_id		INTEGER		NOT NULL,
+	u_nome		VARCHAR(30)	NOT NULL,
+	CONSTRAINT p_u_pk
+		PRIMARY KEY(p_id, u_nome),
+	CONSTRAINT p_id_fk
+		FOREIGN KEY (p_id) REFERENCES produto(p_id),
+	CONSTRAINT u_nome_fk
+		FOREIGN KEY (u_nome) REFERENCES usuario(u_nome)
 );
