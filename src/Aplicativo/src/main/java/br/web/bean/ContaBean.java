e96a74684a3acb05f1b5c62d8a60d3c5993f8982
eduardohk <eduardohk@users.noreply.github.com>
2016-06-20 11:59:55 -0300

    Adicionado os filters para controle do acesso a Visualizacao e Gerencimento de Contas.

--- src/Aplicativo/src/main/java/br/web/bean/ContaBean.java
+++ src/Aplicativo/src/main/java/br/web/bean/ContaBean.java
@@ -14,8 +14,6 @@ import br.jpa.entity.UsuarioConta;
 import br.web.utils.SessionContext;
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
